c18adfa56e762b847eac00619ff54bafd164a149
Dima Hinev <dima.h@index.dev>
2022-07-01 12:08:01 +0300

    gatsby installed

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,24 +3,25 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Bible`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken": "CFPAT-tD6LlSGRl0hIEn_jt5sw5aQ40CKKps6gbok1TdWG94A",
-      "spaceId": ""
-    }
-  }, "gatsby-plugin-sass", {
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": ""
-    }
-  }]
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "U2hMfkEY0eoXIQinGpR8CZMNr6i8VI5Su4lRSRsELTA",
+        spaceId: "8ord7tcapwuw",
+      },
+    },
+    "gatsby-plugin-sass",
+  ],
 };
 
 export default config;
